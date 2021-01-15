ee20e9b81fef30ca3b88bc7502e07b27847186a9
Penson, Kelly <pensonk86@gmail.com>
2021-01-15 12:10:54 -0800

    Created Classes

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public Node RootNode;
+
+        public BinarySearchTree()
+        {
+
+
+        }
+
+        public void CreatNewNode(Node addNode)
+        {
+
+            if (RootNode == null)
+            {
+                RootNode = addNode;
+            }
+            else
+            {
+                Node currentNode = RootNode;
+                while (true)
+                {
+
+                    if (currentNode.NewNode == null)
+                    {
+                        currentNode.NewNode = addNode;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.NewNode;
+
+                    }
+
+
+
+                }
+                
+
+            }
+            
+
+
+
+
+        }
+
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int data;
+        public Node NewNode;
+
+        public Node(int data)
+        {
+
+            this.data = data;
+        }
+       
+
+
+    }
+
+
+}
